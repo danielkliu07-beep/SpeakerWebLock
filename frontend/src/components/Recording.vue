343974674572cc67565e86e2d268e98221e05282
danielkliu07-beep <danielkliu07@gmail.com>
2026-07-25 15:08:09 -0400

    Added recording.js and utility folder

---
+++ frontend/src/components/Recording.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+    import {ref} from 'vue'
+
+</script>
+
+
+<template>
+    
+    <recorder>
+        <button id = "recordBtnStart"></button>
+        <button id = "recordBtnStop"></button>
+
+        <button id = "playRecordedAudio"></button>
+
+
+    </recorder>
+
+
+</template>
+
+
+<style scoped>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,5 @@
+import './assets/main.css' //Base styling for the entire app goes in main.css
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
