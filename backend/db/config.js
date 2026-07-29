f0616be2a3006e2c271c51bf514b5766babde175
danielkliu07-beep <danielkliu07@gmail.com>
2026-07-29 14:40:40 -0400

    Added pg package

---
+++ backend/db/config.js
@@ -0,0 +1,36 @@
+export const config = {
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME,
+    APP_PORT: process.env.APP_PORT
+}
+
+if (process.env === undefined) {
+    throw new Error("Environment variables not defined in env")
+}
+
+if (process.env.DB_USER === undefined) {
+    throw new Error("DB_USER not defined in env")
+}
+
+if (process.env.DB_PASSWORD === undefined) {
+    throw new Error("DB_PASSWORD not defined in env")
+}
+
+if (process.env.DB_HOST === undefined) {
+    throw new Error("DB_HOST not defined in env")
+}
+
+if (process.env.DB_PORT === undefined) {
+    throw new Error("DB_PORT not defined in env")   
+}
+
+if (process.env.DB_NAME === undefined) {
+    throw new Error("DB_NAME not defined in env")   
+}
+
+if (process.env.APP_PORT === undefined) {
+    throw new Error("APP_PORT not defined in env")
+}
---
+++ backend/db/index.js
@@ -0,0 +1,14 @@
+import { Pool, Client } from 'pg'
+import { config } from 'backend/db/config.js'
+
+const connection_information = {
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    host: config.DB_HOST,
+    port: Number(config.DB_PORT),
+    database: config.DB_NAME
+
+}
+
+export const pool = new Pool(connection_information)
+
